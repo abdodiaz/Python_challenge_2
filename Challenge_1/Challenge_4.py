73a98edf86df3f031f2e0900b7b37e74b1c40796
abdelbarre dazia <55512153+abdodiaz@users.noreply.github.com>
2025-07-03 15:19:53 +0100

    update

--- Challenge_1/Challenge_4.py
+++ Challenge_1/Challenge_4.py
@@ -1,4 +1,21 @@
-notes_eleves = { "Amine": 15.5, "Yassine": 19.0, "Reda": 14.2, "Malak": 8.7, "Manal": 20.0, "Ahmed": 7.5,"Saad": 11.3, "Hannae": 9.8 }
-print(notes_eleves)
-notes_eleves_trier=sorted(notes_eleves.items(),)
-print(notes_eleves_trier)
+mon_dict = {
+    "Aziz": 5,
+    "Kamal": 2,
+    "Hicham": 7,
+    "Achraf": 1
+}
+
+cles = list(mon_dict.keys())
+
+for i in range(len(cles)):
+    for j in range(i + 1, len(cles)):
+        if mon_dict[cles[i]] > mon_dict[cles[j]]:
+            temp = cles[i]
+            cles[i] = cles[j]
+            cles[j] = temp
+
+dictionnaire_trie = {}
+for cle in cles:
+    dictionnaire_trie[cle] = mon_dict[cle]
+
+print(dictionnaire_trie)
---
+++ Challenge_2/Challenge_1.py
@@ -0,0 +1,6 @@
+#Gestion d’un compte bancaire en POO
+class CompteBancaire :
+   def __init__(nom_proprietaire ,solde  ):
+      nom_proprietaire=nom_proprietaire
+      solde=0.0
+    
