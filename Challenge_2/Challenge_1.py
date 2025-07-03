4f94766962b983f8e90756a5faaf66d75c83aefb
abdelbarre dazia <55512153+abdodiaz@users.noreply.github.com>
2025-07-03 16:09:57 +0100

    changes

--- Challenge_2/Challenge_1.py
+++ Challenge_2/Challenge_1.py
@@ -1,6 +1,26 @@
 #Gestion d’un compte bancaire en POO
 class CompteBancaire :
-   def __init__(nom_proprietaire ,solde  ):
-      nom_proprietaire=nom_proprietaire
-      solde=0.0
+    def __init__(self,_nom_proprietaire):
+        self._nom_proprietaire=_nom_proprietaire
+        self._solde=0.0
+    def deposer(self,montant):
+      self._solde=+montant
+      return self._solde
     
+    def retirer(self,montant):
+        if montant>self._solde :
+          print("solde < montant")
+        else :
+          result= self._solde-montant
+          return result
+    def afficher_solde(self):
+       solde = self._solde   
+       nom = self._nom_proprietaire  
+       print(f"nom :{nom}, solde : {solde}")
+       
+          
+    
+CompteBancaire = CompteBancaire("ahmed")
+print(CompteBancaire.deposer(float(4)))
+print(CompteBancaire.retirer(float(1)))
+print(CompteBancaire.afficher_solde())
