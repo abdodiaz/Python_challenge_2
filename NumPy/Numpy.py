f808de281ef12bb176c8b409e55495838a20b093
abdelbarre dazia <55512153+abdodiaz@users.noreply.github.com>
2025-07-04 12:10:02 +0100

    numpay challenge

---
+++ NumPy/Numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+temperatures = np.array([20.5, 22.1, 23.8, 25.2, 24.7, 23.9, 22.5, 21.8])
+
+#Challenge : Analyse de Températures
+moyenne = np.mean(temperatures)
+mediane = np.median(temperatures)
+ecart_type = np.std(temperatures)
+
+# Normalisation de Données
+normalized_data = (temperatures - moyenne)/ecart_type
+# print (normalized_data)
+
+#Comparaison de Tableaux
+c1= np.array([5,10,18,22,30])
+c2= np.array([18,6,3,22,10])
+c=np.where(c1 != c2 )
+print(c)
+print(c1[c])
+print(c2[c])
+
+#Challenge : Opérations Matricielles
+
+m1=np.array([5,10,8,6,3,9,4])
+m2=np.array([7,8,6,7,3,9,2])
+mul=np.dot(m1,m2)
+print(mul)
